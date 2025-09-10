de7b4cc92b1ce42fec22244714abbe3e0c7501f6
starshine29 <jose.navarreteb@uniacc.edu>
2025-09-10 13:53:18 -0300

    casos

--- Assets/00_PlayerInputs/Interactions.cs
+++ Assets/00_PlayerInputs/Interactions.cs
@@ -1,13 +1,38 @@
+using System;
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Interactions : MonoBehaviour
 {
     
-    [Header("Health")]
+    private bool key1 = false; [SerializeField]
+    private bool key2 = false; [SerializeField]
+    
+    IEnumerator = 
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start() 
     {
         
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.tag)
+
+        {
+            case "NPC1":
+                Debug.Log("Tengo la llave 1");
+                break;
+
+        
+            case "NPC2":
+                Debug.Log("Tengo la llave 2");
+                break;
+        }
+
     }
 
     // Update is called once per frame
