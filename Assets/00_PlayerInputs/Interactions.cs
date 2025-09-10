bde928d5f7ba4f8d66d2d00c5ea6cce53a554c7f
starshine29 <jose.navarreteb@uniacc.edu>
2025-09-10 13:15:09 -0300

    escenario

---
+++ Assets/00_PlayerInputs/Interactions.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Interactions : MonoBehaviour
+{
+    
+    [Header("Health")]
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start() 
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
